c28035a39ded05e540da5f7f2a4730f5d3fa064c
Oleg V. Kuzmin <inpassor@yandex.com>
2017-03-23 09:36:42 +0300

    Delete main.js

--- src/main.js
+++
@@ -1,9 +0,0 @@
-const functions = [
-    'isUndefined',
-];
-
-for (let i = 0, l = functions.length; i < l; i++) {
-    let f = functions[i],
-        fn = require('./functions/' + f);
-    $[f] = fn[f];
-}
